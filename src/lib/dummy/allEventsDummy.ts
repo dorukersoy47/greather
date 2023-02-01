8f563fbf305f90d5098a5f8dd64a0fb14a6854bc
Barış İnandıoğlu <binandioglu2006@gmail.com>
2023-02-01 05:14:17 +0300

    add missing files

---
+++ src/lib/dummy/allEventsDummy.ts
@@ -0,0 +1,77 @@
+export default [
+	{
+		event: {
+			id: 1,
+			title: "planting trees",
+			where: "Göktürk Fidanlığı, İstanbul",
+			reward: 10,
+			eventPhoto:
+				"https://images.unsplash.com/photo-1569880153113-76e33fc52d5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBsYW50aW5nJTIwdHJlZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60",
+			description:
+				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+			peopleJoined: 1257,
+			peopleTarget: 5000,
+			datesStart: "2023-01-01T21:00:00.000Z",
+			datesGoal: "2022-11-11T00:00:00.000Z",
+			sponsorId: 1,
+			requirements: '"Shovel","Hoe","Boots"',
+		},
+		sponsor: {
+			id: 1,
+			email: "team@trees.com",
+			password: "123456789",
+			name: "TeamTrees",
+			photo: "https://upload.wikimedia.org/wikipedia/commons/7/72/Team_Trees_circle_logo.svg",
+		},
+	},
+	{
+		event: {
+			id: 2,
+			title: "collecting trash",
+			where: "Marmara Seashore, Eminönü",
+			reward: 15,
+			eventPhoto:
+				"https://ocean.si.edu/sites/default/files/styles/photo_full/public/7990147080_72948ec3e3_k.jpg.webp?itok=WghI4msr",
+			description:
+				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+			peopleJoined: 20,
+			peopleTarget: 200,
+			datesStart: "2023-01-31T11:00:34.941Z",
+			datesGoal: "1970-04-01T00:00:00.000Z",
+			sponsorId: 2,
+			requirements: '"Nothing!"',
+		},
+		sponsor: {
+			id: 2,
+			email: "zara@zara.com",
+			password: "1234",
+			name: "Zara",
+			photo: "https://static.dezeen.com/uploads/2019/02/new-zara-logo-sq-1-1024x1024.jpg",
+		},
+	},
+	{
+		event: {
+			id: 3,
+			title: "marching for the ecosystem",
+			where: "İstiklal St., İstanbul",
+			reward: 5,
+			eventPhoto:
+				"https://www.indyturk.com/sites/default/files/styles/1368x911/public/article/main_image/2019/04/15/66586-1492613818.jpg?itok=ve14yt4Z",
+			description:
+				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+			peopleJoined: 68,
+			peopleTarget: 400,
+			datesStart: "2023-01-01T21:00:00.000Z",
+			datesGoal: "2023-05-01T00:00:00.000Z",
+			sponsorId: 3,
+			requirements: '"Love for animals"',
+		},
+		sponsor: {
+			id: 3,
+			email: "google@google.com",
+			password: "1234",
+			name: "Google",
+			photo: "https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=320&h=320&fit=crop",
+		},
+	},
+];
