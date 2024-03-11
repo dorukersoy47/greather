c1e9265a6229f8543ca184b6c1e173d13cac97c3
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2024-03-11 21:25:20 +0300

    Update allEventsDummy.ts

--- src/lib/dummy/allEventsDummy.ts
+++ src/lib/dummy/allEventsDummy.ts
@@ -31,7 +31,7 @@ export default [
 			where: "Marmara Seashore, Eminönü",
 			reward: 15,
 			eventPhoto:
-				"https://ocean.si.edu/sites/default/files/styles/photo_full/public/7990147080_72948ec3e3_k.jpg.webp?itok=WghI4msr",
+				"https://images.unsplash.com/photo-1514955180492-deb713a7e66e?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 			description:
 				"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 			peopleJoined: 20,
